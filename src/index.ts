cc77f731daccfbd022f3ef168d25e1ac6cf6df60
evgeniy-kolmak <evgeniy.kolmak@gmail.com>
2023-03-16 19:21:11 +0300

    make start page

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import './sass/style.sass';
 import './index.html';
 
-(): void => console.log('successful development');
+console.log(
+  ' %c🌌Lucky flying, friend!',
+  'color: #d5cad9; background: linear-gradient(to right, #41295a, #2f0743); font-size: 25px; padding: 6px; border-radius: 2px;'
+);
+
